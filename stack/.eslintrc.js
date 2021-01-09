10e47e2aa1d8402c9147e808c3f41a7c40b4d9ac
Gabe Rogan <rogangabe@gmail.com>
2021-01-09 20:43:08 +0000

    better eslint and default route cdk

---
+++ stack/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
+    },
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  },
+  'ignorePatterns': ['build']
+}
--- stack/index.js
+++ stack/index.js
@@ -1,192 +1,140 @@
-#!/usr/bin/env node
-const {AssetCode, Function, Runtime} = require("@aws-cdk/aws-lambda")
-const {CfnApi, CfnDeployment, CfnIntegration, CfnRoute, CfnStage} = require("@aws-cdk/aws-apigatewayv2")
+const {AssetCode, Function, Runtime} = require('@aws-cdk/aws-lambda')
+const {CfnApi, CfnDeployment, CfnIntegration, CfnRoute, CfnStage} = require('@aws-cdk/aws-apigatewayv2')
 const {App, ConcreteDependable, Duration, RemovalPolicy, Stack} = require('@aws-cdk/core')
-const {Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal} = require("@aws-cdk/aws-iam")
-const {AttributeType, Table} = require("@aws-cdk/aws-dynamodb")
+const {Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal} = require('@aws-cdk/aws-iam')
+const {AttributeType, Table, BillingMode} = require('@aws-cdk/aws-dynamodb')
 const config = require('./config.json')
 
-class ChatAppStack extends Stack {
-    constructor(scope, id, props) {
-        super(scope, id, props);
-        const tableName = "simplechat_connections";
+class WebsocketDynamoDBStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props)
 
-        // initialise api
-        const name = id + "-api"
-        const api = new CfnApi(this, name, {
-            name: "ChatAppApi",
-            protocolType: "WEBSOCKET",
-            routeSelectionExpression: "$request.body.action",
-        });
-        const table = new Table(this, `${name}-table`, {
-            tableName: tableName,
-            partitionKey: {
-                name: "connectionId",
-                type: AttributeType.STRING,
-            },
-            readCapacity: 5,
-            writeCapacity: 5,
-            removalPolicy: RemovalPolicy.DESTROY
-        });
+    const tableName = 'docs'
 
-        // initialise lambda and permissions
+    // Initialize API
+  
+    const name = id + '-api'
+    const api = new CfnApi(this, name, {
+      name: 'WebsocketApi',
+      protocolType: 'WEBSOCKET',
+      routeSelectionExpression: '$request.body.action',
+    })
+    const table = new Table(this, `${name}-table`, {
+      partitionKey: {
+        name: 'PartitionKey',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'SortKey',
+        type: AttributeType.STRING,
+      },
+      tableName,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY, // TODO for prod use RETAIN
+    })
 
-        const lambdaPolicy = new PolicyStatement({
-            actions: [
-                "dynamodb:GetItem",
-                "dynamodb:DeleteItem",
-                "dynamodb:PutItem",
-                "dynamodb:Scan",
-                "dynamodb:Query",
-                "dynamodb:UpdateItem",
-                "dynamodb:BatchWriteItem",
-                "dynamodb:BatchGetItem",
-                "dynamodb:DescribeTable",
-                "dynamodb:ConditionCheckItem"
-            ],
-            resources: [table.tableArn]
-        });
+    // Initialize lambda and permissions
 
-        const connectLambdaRole = new Role(this, "connectLambdaRole", {
-            assumedBy: new ServicePrincipal("lambda.amazonaws.com")
+    const lambdaPolicy = new PolicyStatement({
+      actions: ['dynamodb:*'],
+      resources: [table.tableArn],
+    })
+
+    const messageLambdaRole = new Role(this, `${name}-message-lambda-role`, {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    })
+    messageLambdaRole.addToPolicy(lambdaPolicy)
+    messageLambdaRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'))
+
+    const messageFunc = new Function(this, `${name}-message-lambda`, {
+      code: new AssetCode('../server/build'),
+      handler: 'handler/aws.handler',
+      runtime: Runtime.NODEJS_12_X,
+      timeout: Duration.seconds(300),
+      memorySize: 256,
+      role: messageLambdaRole,
+      // CloudFormation tries to delete lambda before moving alias, this fixes the error:
+      currentVersionOptions: {
+        removalPolicy: RemovalPolicy.RETAIN
+      },
+      initialPolicy: [
+        new PolicyStatement({
+          actions: [
+            'execute-api:ManageConnections'
+          ],
+          resources: [
+            'arn:aws:execute-api:' + config['region'] + ':' + config['account_id'] + ':' + api.ref + '/*'
+          ],
+          effect: Effect.ALLOW,
         })
-        connectLambdaRole.addToPolicy(lambdaPolicy)
-        connectLambdaRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+      ],
+      environment: {
+        TABLE_NAME: tableName,
+      }
+    })
 
-        const disconnectLambdaRole = new Role(this, "disconnectLambdaRole", {
-            assumedBy: new ServicePrincipal("lambda.amazonaws.com")
-        })
-        disconnectLambdaRole.addToPolicy(lambdaPolicy)
-        disconnectLambdaRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+    // Lambda autoscaling (destroy cold starts)
 
-        const messageLambdaRole = new Role(this, "messageLambdaRole", {
-            assumedBy: new ServicePrincipal("lambda.amazonaws.com")
-        })
-        messageLambdaRole.addToPolicy(lambdaPolicy)
-        messageLambdaRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
+    messageFunc.currentVersion.addAlias(id+'-message-lambda-alias')
 
-        const connectFunc = new Function(this, 'connect-lambda', {
-            code: new AssetCode('./onconnect'),
-            handler: 'app.handler',
-            runtime: Runtime.NODEJS_12_X,
-            timeout: Duration.seconds(300),
-            memorySize: 256,
-            role: connectLambdaRole,
-            environment: {
-                "TABLE_NAME": tableName,
-            }
-        });
+    // const autoScale = alias.addAutoScaling({
+    //   maxCapacity: 10,
+    //   minCapacity: 2,
+    // })
+    
+    // autoScale.scaleOnUtilization({
+    //   utilizationTarget: 0.5,
+    //   policyName: id+'-lambda-scaler',
+    // })
 
-        const disconnectFunc = new Function(this, 'disconnect-lambda', {
-            code: new AssetCode('./ondisconnect'),
-            handler: 'app.handler',
-            runtime: Runtime.NODEJS_12_X,
-            timeout: Duration.seconds(300),
-            memorySize: 256,
-            role: disconnectLambdaRole,
-            environment: {
-                "TABLE_NAME": tableName,
-            }
-        });
+    // Access role for the socket api to access the socket lambda
+  
+    const policy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [
+        messageFunc.functionArn,
+      ],
+      actions: ['lambda:InvokeFunction'],
+    })
 
-        const messageFunc = new Function(this, 'message-lambda', {
-            code: new AssetCode('./sendmessage'),
-            handler: 'app.handler',
-            runtime: Runtime.NODEJS_12_X,
-            timeout: Duration.seconds(300),
-            memorySize: 256,
-            role: messageLambdaRole,
-            initialPolicy: [
-                new PolicyStatement({
-                    actions: [
-                        'execute-api:ManageConnections'
-                    ],
-                    resources: [
-                        "arn:aws:execute-api:" + config["region"] + ":" + config["account_id"] + ":" + api.ref + "/*"
-                    ],
-                    effect: Effect.ALLOW,
-                })
-            ],
-            environment: {
-                "TABLE_NAME": tableName,
-            }
-        });
+    const role = new Role(this, `${name}-iam-role`, {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
+    })
+    role.addToPolicy(policy)
 
-        // access role for the socket api to access the socket lambda
-        const policy = new PolicyStatement({
-            effect: Effect.ALLOW,
-            resources: [
-                connectFunc.functionArn,
-                disconnectFunc.functionArn,
-                messageFunc.functionArn
-            ],
-            actions: ["lambda:InvokeFunction"]
-        });
+    // Integrate lambda with Websocket API
+  
+    const messageIntegration = new CfnIntegration(this, `${name}-message-route-lambda-integration`, {
+      apiId: api.ref,
+      integrationType: 'AWS_PROXY',
+      integrationUri: 'arn:aws:apigateway:' + config['region'] + ':lambda:path/2015-03-31/functions/' + messageFunc.functionArn + '/invocations',
+      credentialsArn: role.roleArn,
+    })
 
-        const role = new Role(this, `${name}-iam-role`, {
-            assumedBy: new ServicePrincipal("apigateway.amazonaws.com")
-        });
-        role.addToPolicy(policy);
+    const messageRoute = new CfnRoute(this, `${name}-message-route`, {
+      apiId: api.ref,
+      routeKey: '$default',
+      authorizationType: 'NONE',
+      target: 'integrations/' + messageIntegration.ref,
+    })
 
-        // lambda integration
-        const connectIntegration = new CfnIntegration(this, "connect-lambda-integration", {
-            apiId: api.ref,
-            integrationType: "AWS_PROXY",
-            integrationUri: "arn:aws:apigateway:" + config["region"] + ":lambda:path/2015-03-31/functions/" + connectFunc.functionArn + "/invocations",
-            credentialsArn: role.roleArn,
-        })
-        const disconnectIntegration = new CfnIntegration(this, "disconnect-lambda-integration", {
-            apiId: api.ref,
-            integrationType: "AWS_PROXY",
-            integrationUri: "arn:aws:apigateway:" + config["region"] + ":lambda:path/2015-03-31/functions/" + disconnectFunc.functionArn + "/invocations",
-            credentialsArn: role.roleArn
-        })
-        const messageIntegration = new CfnIntegration(this, "message-lambda-integration", {
-            apiId: api.ref,
-            integrationType: "AWS_PROXY",
-            integrationUri: "arn:aws:apigateway:" + config["region"] + ":lambda:path/2015-03-31/functions/" + messageFunc.functionArn + "/invocations",
-            credentialsArn: role.roleArn
-        })
+    const deployment = new CfnDeployment(this, `${name}-deployment`, {
+      apiId: api.ref,
+    })
 
-        const connectRoute = new CfnRoute(this, "connect-route", {
-            apiId: api.ref,
-            routeKey: "$connect",
-            authorizationType: "NONE",
-            target: "integrations/" + connectIntegration.ref,
-        });
+    new CfnStage(this, `${name}-stage`, {
+      apiId: api.ref,
+      autoDeploy: true,
+      deploymentId: deployment.ref,
+      stageName: config['stage'],
+    })
 
-        const disconnectRoute = new CfnRoute(this, "disconnect-route", {
-            apiId: api.ref,
-            routeKey: "$disconnect",
-            authorizationType: "NONE",
-            target: "integrations/" + disconnectIntegration.ref,
-        });
-
-        const messageRoute = new CfnRoute(this, "message-route", {
-            apiId: api.ref,
-            routeKey: "sendmessage",
-            authorizationType: "NONE",
-            target: "integrations/" + messageIntegration.ref,
-        });
-
-        const deployment = new CfnDeployment(this, `${name}-deployment`, {
-            apiId: api.ref
-        });
-
-        new CfnStage(this, `${name}-stage`, {
-            apiId: api.ref,
-            autoDeploy: true,
-            deploymentId: deployment.ref,
-            stageName: "dev"
-        });
-
-        const dependencies = new ConcreteDependable();
-        dependencies.add(connectRoute)
-        dependencies.add(disconnectRoute)
-        dependencies.add(messageRoute)
-        deployment.node.addDependency(dependencies);
-    }
+    const dependencies = new ConcreteDependable()
+    dependencies.add(messageRoute)
+    deployment.node.addDependency(dependencies)
+  }
 }
-const app = new App();
-new ChatAppStack(app, `yjs`);
-app.synth();
+
+const app = new App()
+
+new WebsocketDynamoDBStack(app, 'yjs')
