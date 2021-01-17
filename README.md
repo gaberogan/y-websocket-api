783a9dc1dc8702b08fe19b185bb855ad86e0d4ef
Gabe Rogan <rogangabe@gmail.com>
2021-01-17 22:54:21 +0000

    local setup and server fixes

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ This is a demo of YJS working with AWS Websocket API and DynamoDB. The intent is
 ## Getting Started
 The `client` folder is a frontend demo of YJS with SlateJS. You can initialize `client` with `npm i && npm start`. It depends on the `server` folder which has a local version of the YJS for AWS backend, based off y-websocket. You can initialize `server` with `npm i && npm start`. The `stack` folder is the full CDK stack for the `server`. Run `npm i` and `npm i -g aws-cdk` then use the CDK CLI to deploy the infrastructure.
 
+## Docker
+
+with WSL2 see fix here https://github.com/docker/compose/issues/7495#issuecomment-649035078
+
+```sh
+cd local-db
+docker-compose up # run local dynamodb server
+npm run setup # create tables
+```
+
+Debug
+```sh
+aws dynamodb list-tables --endpoint-url http://localhost:8000
+aws dynamodb scan --table-name docs --endpoint-url http://localhost:8000
+```
+
 ## Deploy
 TBD
 
--- server/handler/aws.js
+++ server/handler/aws.js
@@ -1,88 +1,106 @@
 // @ts-ignore
 import syncProtocol from 'y-protocols/dist/sync.cjs'
 // @ts-ignore
-import awarenessProtocol from 'y-protocols/dist/awareness.cjs'
-// @ts-ignore
 import encoding from 'lib0/dist/encoding.cjs'
 // @ts-ignore
 import decoding from 'lib0/dist/decoding.cjs'
-import PostToConnection from 'aws-post-to-connection'
-import { addConnection, getConnections, removeConnection, getOrCreateDoc } from '../db/aws'
+import { addConnection, getConnectionIds, removeConnection, getOrCreateDoc, updateDoc } from '../db/aws.js'
+import ws from 'aws-lambda-ws-server'
 
 const messageSync = 0
 const messageAwareness = 1
 
-// TODO make sure input/output are correct (b64?) format and that it parses to uint8array properly
-// TODO setup local dynamodb & https://www.npmjs.com/package/aws-lambda-ws-server
+// TODO make sure input/output are correct (b64?) format and that it parses
+// to uint8array properly (esp getOrCreateDoc, updateDoc)
 
-export const handler = async (event, context) => { // eslint-disable-line
-  const postToSameGateway = PostToConnection(event)
-  const { connectionId, routeKey } = event.requestContext
-  const docName = event.queryStringParameters.doc
+export const handler = ws(
+  ws.handler({
+    // Connect
+    async connect ({ id, event, context }) {
+      const docName = event.queryStringParameters.doc
+      console.log('connection %s', id)
 
-  // handle new connection
-  if (routeKey === '$connect') {
-    // handle connectionId
-    await addConnection(docName, connectionId)
+      await addConnection(docName, id)
 
-    // get doc from db
-    // create new doc with no updates if no doc exists
-    const doc = await getOrCreateDoc(docName)
+      // get doc from db
+      // create new doc with no updates if no doc exists
+      const doc = await getOrCreateDoc(docName)
 
-    // writeSyncStep1 (send sv)
-    const encoder = encoding.createEncoder()
-    encoding.writeVarUint(encoder, messageSync)
-    syncProtocol.writeSyncStep1(encoder, doc)
-    await postToSameGateway({ message: encoding.toUint8Array(encoder) }, connectionId)
+      // writeSyncStep1 (send sv)
+      const encoder = encoding.createEncoder()
+      encoding.writeVarUint(encoder, messageSync)
+      syncProtocol.writeSyncStep1(encoder, doc)
+      await context.postToConnection({ message: encoding.toUint8Array(encoder) }, id)
+
+      return { statusCode: 200 }
+    },
   
-    // DONE
-    return
-  }
-    
-  // handle disconnection
-  if (routeKey === '$disconnect') {
+    // Disconnect
+    async disconnect ({ id, event }) {
+      const docName = event.queryStringParameters.doc
+      console.log('disconnect %s', id)
 
-    // handle connectionId
-    await removeConnection(docName, connectionId)
+      await removeConnection(docName, id)
 
-    // DONE
-    return
-  }
-    
-  // $default handler
+      return { statusCode: 200 }
+    },
+  
+    // Message
+    async default ({ message, id, event, context }) {
+      const docName = event.queryStringParameters.doc
+      console.log('message', message, id)
 
-  // handle connectionId
-  const connections = await getConnections(docName)
+      const connectionIds = await getConnectionIds(docName)
+      const otherConnectionIds = connectionIds.filter(_ => _ !== id)
+      const broadcast = ({ message }) => {
+        return Promise.all(otherConnectionIds.map(id => {
+          return context.postToConnection({ message }, id)
+            .catch(() => removeConnection(docName, id))
+        }))
+      }
 
-  // (leveraging readSyncMessage)
-  // on sync1, use getDoc+message to send back update defined by writeSyncStep2
-  // on sync2/update, append to db and broadcastALL
-  // on awareness, broadcastALL
-  const message = new Uint8Array(event.body)
-  const doc = await getOrCreateDoc(docName)
+      message = new Uint8Array(message)
+      const doc = await getOrCreateDoc(docName)
 
-  const encoder = encoding.createEncoder()
-  const decoder = decoding.createDecoder(message)
-  const messageType = decoding.readVarUint(decoder)
-  switch (messageType) {
-  // Case sync1: Read SyncStep1 message and reply with SyncStep2 (write input to output and format as syncstep2 to all clientsf)
-  // Case sync2 or yjsUpdate: Read and apply Structs and then DeleteStore to a y instance (append to db)
-  case messageSync:
-    encoding.writeVarUint(encoder, messageSync)
-    syncProtocol.readSyncMessage(decoder, encoder, doc, null)
-    if (encoding.length(encoder) > 1) {
-      await postToSameGateway({ message: encoding.toUint8Array(encoder) }, connectionId)
-    }
-    break
-  // Client sent its awareness state. Store locally and emit to allow broadcast to fire
-  // Seems like deletions aren't transmitted, just assumed if the client didn't get an update in the past X mins
-  // Seems like we can just broadcast each update and not worry about storing it.
-  case messageAwareness: {
-    awarenessProtocol.applyAwarenessUpdate(doc.awareness, decoding.readVarUint8Array(decoder), conn)
-    break
-  }
-  }
+      const encoder = encoding.createEncoder()
+      const decoder = decoding.createDecoder(message)
+      const messageType = decoding.readVarUint(decoder)
+  
+      switch (messageType) {
+        // Case sync1: Read SyncStep1 message and reply with SyncStep2 (send doc to client wrt state vector input)
+        // Case sync2 or yjsUpdate: Read and apply Structs and then DeleteStore to a y instance (append to db, send to all clients)
+        case messageSync:
+          encoding.writeVarUint(encoder, messageSync)
 
-  // DONE
-  return
-}
+          // syncProtocol.readSyncMessage
+          const messageType = decoding.readVarUint(decoder)
+          switch (messageType) {
+            case syncProtocol.messageYjsSyncStep1:
+              syncProtocol.readSyncStep1(decoder, encoder, doc)
+              break
+            case syncProtocol.messageYjsSyncStep2:
+            case syncProtocol.messageYjsUpdate:
+              syncProtocol.readSyncStep2(decoder, doc, null)
+              await updateDoc(docName, message)
+              await broadcast({ message })
+              break
+            default:
+              throw new Error('Unknown message type')
+          }
+
+          // Reply with our state
+          if (encoding.length(encoder) > 1) {
+            await context.postToConnection({ message: encoding.toUint8Array(encoder) }, id)
+          }
+
+          break
+        case messageAwareness: {
+          await broadcast({ message })
+          break
+        }
+      }
+
+      return { statusCode: 200 }
+    },
+  })
+)
